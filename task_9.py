2e5750edb128de408e32cb9007e49e6eaec2a20e
Rahim <rahimullah25678@gmail.com>
2024-01-11 21:39:11 +0500

    task done

--- task_9.py
+++ task_9.py
@@ -1,5 +1,25 @@
 # 9. Create a program that counts the occurrences of each word in a text document.
 
+text = open('Interview_Tasks\sample.txt','r')
 
-text_doc = "I am rahim ullah from bannu i was born at jankhel he is nasir he is alsofrom bannu."
+d =dict()
 
+# remove extra spaces
+
+for line in text:
+    line = line.strip()         # remove leading spaces and new line character
+
+    line = line.lower()
+
+    words = line.split(" ")         # convert the lines into the words
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+
+
+for key in d:
+    print(key,":",d[key])            
+
+text.close()
